f89e76e922a7b91bbfb1ef01b6e33cd17b268aa4
Shelby Gallegos <116443555+ShelbyGall@users.noreply.github.com>
2026-04-18 17:52:54 -0700

    populated the minefield with random mines and spaces for non bombs, now just need to add the numbers. might need to use a graph or sum

--- Main.java
+++ Main.java
@@ -6,5 +6,10 @@ public class Main {
         Minesweeper ms = new Minesweeper(9);
         System.out.println(ms.getGridSize());
         ms.printMineField();
+
+        Minesweeper ms1 = new Minesweeper(2);
+        ms1.printMineField();
+        Minesweeper ms2 = new Minesweeper(1);
+
     }
 }
--- Minesweeper.java
+++ Minesweeper.java
@@ -1,9 +1,14 @@
 package minesweeper;
 
+import java.util.HashSet;
+import java.util.Random;
+
+import java.util.Arrays;
+
 public class Minesweeper {
     private int gridSize;
     private int numMines;
-    private int[][] mineField;
+    private String[][] mineField;
 
     public Minesweeper(int grid) {
         this(grid, 5);
@@ -12,9 +17,33 @@ public class Minesweeper {
     public Minesweeper(int grid, int numMines) {
         this.gridSize = grid;
         this.numMines = numMines;
-        this.mineField = new int[this.gridSize][this.gridSize];
+        this.mineField = new String[this.gridSize][this.gridSize];
+
+        for (String[] row : mineField) {
+            Arrays.fill(row, " ");
+        }
+        
+        if (this.gridSize < 2) {
+            throw new IllegalArgumentException("Invalid mine field size");
+        } else if (this.numMines >= this.gridSize * this.gridSize) {
+            throw new IllegalArgumentException("Invalid number of mines inputted");
+        }
 
         // Logic for adding mines in random places in the matrix
+        HashSet<String> minePositions = new HashSet<>();
+        Random rand = new Random();
+        while (minePositions.size() < this.numMines) {
+            int i = rand.nextInt(this.gridSize);
+            int j = rand.nextInt(this.gridSize);
+
+            String posStr = "%d,%d".formatted(i, j);
+            if (minePositions.contains(posStr)) {
+                continue;
+            }
+
+            minePositions.add(posStr);
+            mineField[i][j] = "*";
+        }
         
     }
 
@@ -26,7 +55,7 @@ public class Minesweeper {
         return this.numMines;
     }
 
-    public int[][] getMineField() {
+    public String[][] getMineField() {
         return this.mineField;
     }
 
@@ -36,7 +65,7 @@ public class Minesweeper {
         for (int i = 0; i < this.getGridSize(); i++) {
             System.out.print('|');
             for (int j = 0; j < this.getGridSize(); j++) {
-                System.out.printf("%d|", this.mineField[i][j]);
+                System.out.printf("%s|", this.mineField[i][j]);
             }
             System.out.println("\n" + separator);
         }
